6a558ad25de5096de1ddd5c10ba3f979c8e94cc3
win5948 <win5948@gmail.com>
2023-11-28 00:00:01 +0900

    start

---
+++ src/main/java/com/ll/mission_sbb/MissionSbbApplication.java
@@ -0,0 +1,13 @@
+package com.ll.mission_sbb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MissionSbbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MissionSbbApplication.class, args);
+    }
+
+}
