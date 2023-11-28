659808cf34f9a204dd7efc1d6b582576fd3aaae1
win5948 <win5948@gmail.com>
2023-11-28 14:27:57 +0900

    findBySubjectAndContent

--- src/test/java/com/ll/mission_sbb/MissionSbbApplicationTests.java
+++ src/test/java/com/ll/mission_sbb/MissionSbbApplicationTests.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -16,7 +14,8 @@ class MissionSbbApplicationTests {
 
     @Test
     void testJpa() {
-        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        Question q = this.questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, q.getId());
     }
 }
