4bc5248d8087f243d53038f0c4832e6f173a1887
win5948 <win5948@gmail.com>
2023-11-28 14:25:00 +0900

    findById

--- src/test/java/com/ll/mission_sbb/MissionSbbApplicationTests.java
+++ src/test/java/com/ll/mission_sbb/MissionSbbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,10 +16,10 @@ class MissionSbbApplicationTests {
 
     @Test
     void testJpa() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
 }
