d1348647bcd860e15ce9b1cd83f698037ce48102
win5948 <win5948@gmail.com>
2023-11-28 16:06:25 +0900

    2-11

---
+++ src/main/java/com/ll/mission_sbb/answer/AnswerService.java
@@ -0,0 +1,23 @@
+package com.ll.mission_sbb.answer;
+
+import com.ll.mission_sbb.question.Question;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+    private final AnswerRepository answerRepository;
+
+
+    public void create(Question question, String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setCreateDate(LocalDateTime.now());
+        answer.setQuestion(question);
+        this.answerRepository.save(answer);
+    }
+}
